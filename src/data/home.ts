1207a1770b01ec62962df8fef6790069e0f4d997
sharms-my <sharms.my@gmail.com>
2024-05-06 21:06:36 +0800

    added sticky category pills

---
+++ src/data/home.ts
@@ -0,0 +1,17 @@
+export const categories = [
+    "All",
+    "JavaScript",
+    "TypeScript",
+    "Programming",
+    "Sleeping",
+    "Day Dreaming",
+    "Napping",
+    "React",
+    "Next.js",
+    "Functional Programming",
+    "Object Oriented Programming",
+    "Frontend Web Development",
+    "Backend Web Development",
+    "Web Development",
+    "Coding",
+  ]
